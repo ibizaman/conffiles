83301e9c7ff05a12d3e160c44ed73cc936ea9e5d
Pierre Penninckx <ibizapeanut@gmail.com>
2014-12-14 02:44:45 +0100

    enhance add/update script

--- add_bundle.sh
+++ add_bundle.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
 function usage() {
-    echo "$0 repo url [repo_branch]
+    echo "$0 name url [repo_branch]
 
 Add a remote git repository as a pathogen plugin thanks to git subtree
 
-repo : local name of repo
+name : local name of the plugin
 url : repository location
 repo_branch : branch of the remote"
 }
 
 test -z "$1" || test -z "$2" && usage && exit 1
-branch="$1"
+plugin="$1"
 install_dir=bundle/"$1"
 
-repo_name="$1"
 repo_location="$2"
-repo_branch=${3:-"master"}
+repo_branch="$plugin"/${3:-"master"}
+remotes_file=remotes
 
-git remote add "$repo_name" "$repo_location" \
-&& git fetch "$repo_name" \
-&& git branch "$branch" "$repo_name"/"$repo_branch" \
+existing_plugin=$(awk -F " " '/^'$plugin'/ {print $2}' $remotes_file)
+if [ ! -z "$existing_plugin" ]; then
+    echo "Plugin already exists in remotes file, aborting."
+    exit 1
+fi
+
+git remote add "$plugin" "$repo_location" \
+&& git fetch "$plugin" \
 && mkdir -p "$install_dir" \
-&& git read-tree --prefix="$install_dir" -u "$branch" \
+&& git read-tree --prefix="$install_dir" -u "$plugin/$repo_branch" \
+&& echo "$plugin $repo_location $repo_branch" >> "$remotes_file" \
 || exit 1
--- update_bundle.sh
+++ update_bundle.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
-
 function usage() {
-    echo "$0 bundle
+    echo "$0 name
 
 Updates a pathogen bundle using git subtree
 
-bundle : name of plugin"
+name : name of the plugin"
 }
 
 test -z "$1" && usage && exit 1
-branch="$1"
+plugin="$1"
 
-git checkout "$branch" \
-&& git pull \
-&& git checkout "@{-1}" \
-&& git merge --squash -s subtree --no-commit "$branch" \
-|| exit 1
+remotes_file=remotes
+
+if [ ! -f "$remotes_file" ]; then
+    echo "Cannot find $remotes_file; cannot update bundle."
+    exit 1
+fi
+
+repo_location=$(awk -F " " '/^'$plugin'/ {print $2}' $remotes_file)
+repo_branch=$(awk -F " " '/^'$plugin'/ {print $3}' $remotes_file)
+if [ -z "$repo_location" ]; then
+    echo "No bundle named $plugin in remotes file."
+    exit 1
+fi
+
+if ! $(git remote | grep -q $plugin); then
+    git remote add "$plugin" "$repo_location" || exit 1
+fi
+
+git fetch "$plugin" || exit 1
+#git branch -f "$plugin" "$plugin/$repo_branch" || exit 1
+
+echo "Merging $plugin branch."
+git merge --squash -s subtree --no-commit "$plugin/$repo_branch" || exit 1
