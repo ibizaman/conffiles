c399b309a7bc5dbff5252c17a0ad67f1dbd37ca3
ibizaman <ibizapeanut@gmail.com>
2018-11-13 09:27:16 -0800

    update install.sh to not install vim

--- install.sh
+++ install.sh
@@ -13,25 +13,25 @@ cd
 if [ ! -d .vim ]; then
     git clone https://github.com/ibizaman/conffiles.git .vim
 fi
-ln -s .vim/vimrc .vimrc
-ln -s .vim/gitignore .gitignore
-ln -s .vim/gitconfig .gitconfig
-ln -s .vim/zshrc .zshrc
-ln -s .vim/zsh .zsh
-ln -s .vim/tmux.conf .tmux.conf
-ln -s .vim/Xresources .Xresources
-ln -s .vim/xinitrc .xinitrc
+#ln -s .vim/vimrc .vimrc
+Ln -s ~/.vim/gitignore .gitignore
+ln -s ~/.vim/gitconfig .gitconfig
+ln -s ~/.vim/zshrc .zshrc
+ln -s ~/.vim/zsh .zsh
+ln -s ~/.vim/tmux.conf .tmux.conf
+ln -s ~/.vim/Xresources .Xresources
+ln -s ~/.vim/xinitrc .xinitrc
 mkdir -p .config
 ln -s ~/.vim/i3 .config
 ln -s ~/.vim/i3status .config
 ln -s ~/.vim/dunst .config
 ln -s ~/.vim/bin .
-ln -s ~/.vim/vifmrc .config/vifm/vifmrc
-ln -s .vim/muttrc .muttrc
-ln -s .vim/mailcap .mailcap
+#ln -s ~/.vim/vifmrc .config/vifm/vifmrc
+ln -s ~/.vim/muttrc .muttrc
+ln -s ~/.vim/mailcap .mailcap
 ln -s ~/.vim/psqlrc .psqlrc
 ln -s ~/.vim/emacs .emacs
-cd .vim
-git submodule update --init --recursive -- bundle/youcompleteme
-cd bundle/youcompleteme
-./install.sh
+#cd .vim
+#git submodule update --init --recursive -- bundle/youcompleteme
+#cd bundle/youcompleteme
+#./install.sh
