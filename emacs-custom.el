8f6e966df044cc686dc56e4d0c034165f4db709c
ibizaman <ibizapeanut@gmail.com>
2019-04-28 00:01:58 -0700

    add slashes to adaptive-fill-regexp

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(adaptive-fill-regexp "[ 	]*\\([-–/!|#%;>*·•‣⁃◦]+[ 	]*\\)*")
  '(auth-source-do-cache t)
  '(company-go-show-annotation t)
  '(compilation-auto-jump-to-first-error t)
